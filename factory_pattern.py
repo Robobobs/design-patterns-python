0c534267be6efa026531023ed14d5c0e6bda8699
Bobby P <pearson_2006@hotmail.com>
2021-10-21 08:30:08 +0100

    finished commit of factory_pattern.py

---
+++ factory_pattern.py
@@ -0,0 +1,68 @@
+'''Example script showcasing the factory design pattern for a generic Product'''
+
+from abc import ABCMeta, abstractmethod
+
+class IProduct(metaclass=ABCMeta):      # Abstract classes cannot be instantiated.
+    '''Defines abstract Product class'''
+
+    @staticmethod                       # Removes need for 'self' argument. Self not needed as method is not implemented.
+    @abstractmethod                     # Ensures the inheriting class must override this method during instantiation.
+    def product_method():                 # Defined common interface method.
+        '''Static interface method only'''
+
+
+class ProductA(IProduct):
+    '''Class for Product A'''
+    def __init__(self):
+        self.name = 'Product A'
+        self.description = f'{self.name} is a small product'
+
+    def product_method(self):
+        print('Generic method call for Product A')
+
+class ProductB(IProduct):
+    '''Class for Product B'''
+    def __init__(self):
+        self.name = 'Product B'
+        self.description = f'{self.name} is a medium product'
+
+    def product_method(self):
+        print('Generic method call for Product B')
+
+class ProductC(IProduct):
+    '''Class for Product C'''
+    def __init__(self):
+        self.name = 'Product C'
+        self.description = f'{self.name} is a large product'
+
+    def product_method(self):
+        print('Generic method call for Product C')
+
+
+class ProductFactory:
+    '''Product factory class'''
+    
+    def new_product(self, user_input):
+        '''Defines a new product via argument(user_input)'''
+        if user_input == 'A':
+            return ProductA()
+        elif user_input == 'B':
+            return ProductB()
+        elif user_input == 'C':
+            return ProductC()
+        else:
+            print('\nERROR: Invalid product type!')
+            return None
+  
+
+if __name__ == "__main__":
+    user_input = input('Choose a product type (A, B or C): ')
+    product1 = ProductFactory().new_product(user_input.upper())
+    
+    # ------------------------------ TEST CASE ----------------------------------- #
+    try:
+        print(f"\nName: {product1.name}")
+        print(f"Description: {product1.description}")
+        product1.product_method()
+    except AttributeError:
+        print('Product attribute(s) not found...')
