8ffdaa35fbd4560a1de69765aef4a1304fd00b94
Bobby P <pearson_2006@hotmail.com>
2021-11-05 08:46:12 +0000

    renamed classes, added food dictionary - need to refactor

--- facade_pattern.py
+++ facade_pattern.py
@@ -2,16 +2,13 @@
 
 import time
 
-class CheckDrinkChoice:
+class ChoiceValidator:
     '''Checks if drink selection is available'''
     def check_drink_choice(self, drink_choice, drinks):
-        if drink_choice in drinks.keys():
-            return True
-        else:
-            False 
-   
+        return True if drink_choice in drinks.keys() else False
+            
 
-class CheckStock:
+class Stock:
     '''Checks if drink selection is in stock'''
     def check_stock_level(self, drink_choice, drinks):     
         for drink, stock_num in drinks.items():
@@ -20,7 +17,7 @@ class CheckStock:
         return False            
 
     
-class Dispense:
+class Dispenser:
     '''Initiates physical vending (activates dispensing pump)'''    
     def dispense(self):
         print('Dispensing drink...')
@@ -30,23 +27,41 @@ class Dispense:
 class VendingFacade:
     '''Facade for vending machine sub processes'''    
     def __init__(self):
-        self.choice = CheckDrinkChoice()
-        self.stock = CheckStock()
-        self.dispense = Dispense()
+        self.choice = ChoiceValidator()
+        self.stock = Stock()
+        self.dispenser = Dispenser()
 
     def dispense_drink(self, drink_choice, drinks):
         choice = self.choice.check_drink_choice(drink_choice, drinks)
         stock = self.stock.check_stock_level(drink_choice, drinks)
 
         if choice == True and stock > 0:
-            self.dispense.dispense()
+            self.dispenser.dispense()
             print('Dispensing complete. Enjoy your drink!')
         else:
             print(f'{drink_choice.title()} is not available or out of stock...')
             return None
 
 
+
+    # drinks dict = {}
+
+    # function to add new drink add_new_drink(self, drink, price, stock)
+
+    # initialise Facade with self.add_new_drink(--,--,--)
+
+    
+
 if __name__ == '__main__':
+    
+    #   Client specific foods incl. stock values
+    food = {
+            'chocolate': 2,
+            'banana': 7,
+            'biscuit': 0,
+            'bulgarian fruit cake': 1,
+            'apple': 9,
+            }
     
     #   Client specific drinks incl. stock values
     drinks = {
@@ -58,7 +73,7 @@ if __name__ == '__main__':
             }
 
     #   User input and vending process
-    drink_choice = input('Select your drink: ')
+    drink_choice = input('Please select your food or drink: ')
     vend = VendingFacade()
     vend.dispense_drink(drink_choice.lower(), drinks)
     
